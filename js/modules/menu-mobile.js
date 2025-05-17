6cbed18c26ad79d689564b9e4986dadf9d7cce44
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-05-17 12:59:35 -0300

    upp

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,13 +1,13 @@
 export default function initMenuMobile(){
     const btn = document.querySelector('[data-menu="button"]');
-    const menuList = document.querySelectorAll('[data-menu="list"]');
+    const menuList = document.querySelector('[data-menu="list"]');
+    const redesList = document.querySelector('#lista-redes');
     console.log(menuList)
 
-    function openMenu({ target }){
-        menuList.forEach((lista)=>{
-            lista.classList.add('active')
-        })
+    function openMenu(){
         menuList.classList.add('active')
+        redesList.classList.add('active')
+        this.classList.add('active')
     }
 
 btn.addEventListener('click', openMenu);
