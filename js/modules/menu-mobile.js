1309d59e229b9a05f1ef8c5196c014e7043e4073
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-05-17 12:29:20 -0300

    upp

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,3 +1,15 @@
 export default function initMenuMobile(){
-    
+    const btn = document.querySelector('[data-menu="button"]');
+    const menuList = document.querySelectorAll('[data-menu="list"]');
+    console.log(menuList)
+
+    function openMenu({ target }){
+        menuList.forEach((lista)=>{
+            lista.classList.add('active')
+        })
+        menuList.classList.add('active')
+    }
+
+btn.addEventListener('click', openMenu);
 }
+
