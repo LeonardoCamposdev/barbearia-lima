4816849a1696672f9ccb57ef5ffe0ea572d89235
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-05-22 22:17:41 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 import initMenuMobile from "./modules/menu-mobile.js";
-import initOutsideClick from "./modules/outsideClick.js";
 import { initSlide1, initSlide2 } from "./modules/slide.js";
+import initPluginScroll from "./modules/plugin-scroll.js";
 import { initAnimacaoScrollLeft, initAnimacaoScrollRight, initAnimacaoScrollUp, initAnimacaoScrollDown } from "./modules/animacao-scroll.js";
 
 initMenuMobile();
+initPluginScroll();
 initAnimacaoScrollLeft();
 initAnimacaoScrollRight();
 initAnimacaoScrollUp();
